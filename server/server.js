937b45da6fdebaddf813fda060f263b8fb74eedf
Noah Harris <noah.christopher.harris@gmail.com>
2013-12-17 13:30:30 -0800

    Successfully deployed to Heroku

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@
 var http = require('http');
 var handlers = require('./request-handlers.js');
 
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 8000;  //adjusted for heroku
 var ip = '127.0.0.1';
 
 var server = http.createServer(handlers.mainHandler);
-server.listen(port);
+server.listen(port);   //adjusted for heroku
 //console.log('Server listening at ' + ip + ':' + port);
