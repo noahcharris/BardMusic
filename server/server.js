43ae55cbdca6e1a855995e1e1df02622c1872aed
Noah Harris <noah.christopher.harris@gmail.com>
2013-12-17 13:20:04 -0800

    going to try deploying to heroku

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@
 var http = require('http');
 var handlers = require('./request-handlers.js');
 
-var port = 8080;
+var port = process.env.PORT || 8000;
 var ip = '127.0.0.1';
 
 var server = http.createServer(handlers.mainHandler);
-server.listen(port, ip);
-console.log('Server listening at ' + ip + ':' + port);
+server.listen(port);
+//console.log('Server listening at ' + ip + ':' + port);
