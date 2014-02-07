ac16ba28e8fd9fcfc40393212339d37bd427d7b8
Noah Harris <noah.christopher.harris@gmail.com>
2014-02-07 13:47:49 -0800

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 BardMusic
 =========
+
+This is a prototype for a PR site which catalogues the various student musical projects at Bard College. It currently integrates gCal and Leaflet for event scheduling and venue directions.
--- server/server.js
+++ server/server.js
@@ -6,5 +6,5 @@ var port = process.env.PORT || 8080;  //adjusted for heroku
 var ip = '127.0.0.1';
 
 var server = http.createServer(handlers.mainHandler);
-server.listen(port);   //adjusted for heroku
+server.listen(port, ip);   //adjusted for heroku
 console.log('Server listening at ' + ip + ':' + port);
